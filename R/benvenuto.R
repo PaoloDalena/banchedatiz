a10dc24a127d481dade26948bb8d5b49f3c24d19
Paolo Dalena <paolodalena97@gmail.com>
2019-09-04 16:28:22 +0200

    first commit edit DESCRIPTION add onAttach message

---
+++ R/benvenuto.R
@@ -0,0 +1,4 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Questo pacchetto ti permette di accedere comodamente a tutte le funzioni e i file utili per l'insegnamento Utilizzo Statistico di Banche Dati Economiche Online.
+Buona fortuna!")
+}
